9962bcf2afb852ac26f56f86c7ced8873938be9e
Onur Aslan <onuraslan@gmail.com>
2016-05-29 16:30:33 +0300

    Fix merge

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,6 @@
 
 #[macro_use]
 extern crate log;
-<<<<<<< HEAD
-extern crate rustc_serialize;
-extern crate toml;
-extern crate regex;
-extern crate cargo;
-extern crate postgres;
-extern crate hyper;
-extern crate time;
-extern crate slug;
-
-// Web interface dependencies
-extern crate iron;
-extern crate router;
-extern crate handlebars_iron;
-extern crate staticfile;
-extern crate mount;
-
-pub mod docbuilder;
-pub mod db;
-pub mod web;
-=======
 extern crate cargo;
 extern crate regex;
 extern crate rustc_serialize;
@@ -42,17 +21,11 @@ pub mod db;
 mod build_doc;
 mod copy;
 mod docbuilder;
->>>>>>> 0.2.0
 
 
 /// Version string generated at build time contains last git
 /// commit hash and build date
 pub const BUILD_VERSION: &'static str = concat!(
-<<<<<<< HEAD
-    env!("CARGO_PKG_VERSION"),
-    include!(concat!(env!("OUT_DIR"), "/git_version"))
-=======
     env!("CARGO_PKG_VERSION"), " ",
     include_str!(concat!(env!("OUT_DIR"), "/git_version"))
->>>>>>> 0.2.0
 );
