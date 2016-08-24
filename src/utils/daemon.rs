0222cd38d5fb38552f0179056fe34d9ca3afc7e4
Onur Aslan <onuraslan@gmail.com>
2016-08-24 12:44:00 +0300

    Update daemon

--- src/utils/daemon.rs
+++ src/utils/daemon.rs
@@ -3,14 +3,13 @@
 //! This daemon will start web server, track new packages and build them
 
 
-use std::env;
-use libc::fork;
+use std::{env, thread};
 use std::process::exit;
 use std::fs::File;
 use std::io::Write;
-use std::thread;
 use std::time::Duration;
 use std::path::PathBuf;
+use libc::fork;
 use time;
 use DocBuilderOptions;
 use DocBuilder;
@@ -29,7 +28,8 @@ pub fn start_daemon() {
         env::var(v).expect("Environment variable not found");
     }
 
-    info!("Starting cratesfyi {} daemon", ::BUILD_VERSION);
+    // check paths once
+    opts().check_paths().unwrap();
 
     // fork the process
     let pid = unsafe { fork() };
@@ -37,18 +37,10 @@ pub fn start_daemon() {
         let mut file = File::create(DAEMON_PID_FILE_PATH).expect("Failed to create pid file");
         writeln!(&mut file, "{}", pid).expect("Failed to write pid");
 
-        info!("cratesfyi daemon started on: {}", pid);
+        info!("cratesfyi {} daemon started on: {}", ::BUILD_VERSION, pid);
         exit(0);
     }
 
-
-    fn opts() -> DocBuilderOptions {
-        let prefix = PathBuf::from(env::var("CRATESFYI_PREFIX")
-                                   .expect("CRATESFYI_PREFIX environment variable not found"));
-        let opts = DocBuilderOptions::from_prefix(prefix);
-        opts.check_paths().unwrap();
-        opts
-    }
 
     // check new crates every 5 minutes
     thread::spawn(move || {
@@ -67,10 +59,28 @@ pub fn start_daemon() {
     thread::spawn(move || {
         loop {
             thread::sleep(Duration::from_secs(180));
+
+            let mut opts = opts();
+            opts.skip_if_exists = true;
+
+            // check lock file
+            if opts.prefix.join("cratesfyi.lock").exists() {
+                warn!("Lock file exits, skipping building new crates");
+            }
+
+            let mut doc_builder = DocBuilder::new(opts);
+            if let Err(e) = doc_builder.load_cache() {
+                error!("Failed to load cache: {}", e);
+                continue;
+            }
+
             debug!("Building new crates");
-            let mut doc_builder = DocBuilder::new(opts());
             if let Err(e) = doc_builder.build_packages_queue() {
                 error!("Failed build new crates: {}", e);
+            }
+
+            if let Err(e) = doc_builder.save_cache() {
+                error!("Failed to save cache: {}", e);
             }
         }
     });
@@ -95,4 +105,12 @@ pub fn start_daemon() {
     // at least start web server
     info!("Starting web server");
     ::start_web_server(None);
+}
+
+
+
+fn opts() -> DocBuilderOptions {
+    let prefix = PathBuf::from(env::var("CRATESFYI_PREFIX")
+                               .expect("CRATESFYI_PREFIX environment variable not found"));
+    DocBuilderOptions::from_prefix(prefix)
 }
