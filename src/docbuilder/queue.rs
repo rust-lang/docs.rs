f254da616c2d0baff8285a71c9a7a300f8108f77
QuietMisdreavus <grey@quietmisdreavus.net>
2019-03-26 17:13:28 -0500

    mark releases as yanked when a yank is detected

--- src/docbuilder/queue.rs
+++ src/docbuilder/queue.rs
@@ -19,12 +19,26 @@ impl DocBuilder {
         // I belive this will fix ordering of queue if we get more than one crate from changes
         changes.reverse();
 
-        for krate in changes.iter().filter(|k| k.kind != ChangeKind::Yanked) {
-            let priority = get_crate_priority(&conn, &krate.name)?;
-            add_crate_to_queue(&conn, &krate.name, &krate.version, priority).ok();
+        for krate in &changes {
+            match krate.kind {
+                ChangeKind::Yanked => {
+                    // FIXME: remove built doc files? set build as failed?
+                    conn.execute(
+                        "UPDATE releases SET yanked = TRUE FROM crates WHERE \
+                                  crates.id = releases.crate_id AND name = $1 AND version = $2",
+                        &[&krate.name, &krate.version],
+                    )
+                    .ok();
+                    debug!("{}-{} yanked", krate.name, krate.version);
+                }
+                ChangeKind::Added => {
+                    let priority = get_crate_priority(&conn, &krate.name)?;
+                    add_crate_to_queue(&conn, &krate.name, &krate.version, priority).ok();
 
-            debug!("{}-{} added into build queue", krate.name, krate.version);
-            add_count += 1;
+                    debug!("{}-{} added into build queue", krate.name, krate.version);
+                    add_count += 1;
+                }
+            }
         }
 
         Ok(add_count)
