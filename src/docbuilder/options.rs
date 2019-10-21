1254a06d5572e79ee9e8fc491811df5527487830
Pietro Albini <pietro@pietroalbini.org>
2019-10-21 10:06:06 +0200

    docbuilder: remove destination from the prefix
    After the previous commit, the destination path is not used anywhere in
    the codebase, so it's safe to remove it.

--- src/docbuilder/options.rs
+++ src/docbuilder/options.rs
@@ -8,7 +8,6 @@ use error::Result;
 pub struct DocBuilderOptions {
     pub keep_build_directory: bool,
     pub prefix: PathBuf,
-    pub destination: PathBuf,
     pub crates_io_index_path: PathBuf,
     pub skip_if_exists: bool,
     pub skip_if_log_exists: bool,
@@ -24,12 +23,11 @@ impl Default for DocBuilderOptions {
 
         let cwd = env::current_dir().unwrap();
 
-        let (prefix, destination, crates_io_index_path) =
+        let (prefix, crates_io_index_path) =
             generate_paths(cwd);
 
         DocBuilderOptions {
             prefix: prefix,
-            destination: destination,
             crates_io_index_path: crates_io_index_path,
 
             keep_build_directory: false,
@@ -46,11 +44,10 @@ impl Default for DocBuilderOptions {
 impl fmt::Debug for DocBuilderOptions {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f,
-               "DocBuilderOptions {{ destination: {:?}, \
+               "DocBuilderOptions {{ \
                 crates_io_index_path: {:?}, \
                 keep_build_directory: {:?}, skip_if_exists: {:?}, \
                 skip_if_log_exists: {:?}, debug: {:?} }}",
-               self.destination,
                self.crates_io_index_path,
                self.keep_build_directory,
                self.skip_if_exists,
@@ -63,11 +60,10 @@ impl fmt::Debug for DocBuilderOptions {
 impl DocBuilderOptions {
     /// Creates new DocBuilderOptions from prefix
     pub fn from_prefix(prefix: PathBuf) -> DocBuilderOptions {
-        let (prefix, destination, crates_io_index_path) =
+        let (prefix, crates_io_index_path) =
             generate_paths(prefix);
         DocBuilderOptions {
             prefix: prefix,
-            destination: destination,
             crates_io_index_path: crates_io_index_path,
 
             ..Default::default()
@@ -76,9 +72,6 @@ impl DocBuilderOptions {
 
 
     pub fn check_paths(&self) -> Result<()> {
-        if !self.destination.exists() {
-            bail!("destination path '{}' does not exist", self.destination.display());
-        }
         if !self.crates_io_index_path.exists() {
             bail!("crates.io-index path '{}' does not exist", self.crates_io_index_path.display());
         }
@@ -88,9 +81,8 @@ impl DocBuilderOptions {
 
 
 
-fn generate_paths(prefix: PathBuf) -> (PathBuf, PathBuf, PathBuf) {
-    let destination = PathBuf::from(&prefix).join("documentations");
+fn generate_paths(prefix: PathBuf) -> (PathBuf, PathBuf) {
     let crates_io_index_path = PathBuf::from(&prefix).join("crates.io-index");
 
-    (prefix, destination, crates_io_index_path)
+    (prefix, crates_io_index_path)
 }
