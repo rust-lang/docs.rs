0611e9009d963ed081347fd27a68c1b193f7bd06
Pietro Albini <pietro@pietroalbini.org>
2020-01-08 11:48:52 +0100

    test: add utility to test the web server
    This allows to ensure the web server returns the correct content, by
    spawning a dummy instance of docs.rs for each test, with the test
    database. It's then possible to run any HTTP request against it, as a
    normal user would do.

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -1,22 +1,14 @@
 mod fakes;
 
+use crate::web::Server;
 use failure::Error;
 use postgres::Connection;
 use std::sync::{Arc, Mutex, MutexGuard};
+use reqwest::{Client, Method, RequestBuilder};
 
 pub(crate) fn with_database(f: impl FnOnce(&TestDatabase) -> Result<(), Error>) {
     let env = TestDatabase::new().expect("failed to initialize the environment");
-
-    if let Err(err) = f(&env) {
-        eprintln!("the test failed: {}", err);
-        for cause in err.iter_causes() {
-            eprintln!("  caused by: {}", cause);
-        }
-
-        eprintln!("{}", err.backtrace());
-
-        panic!("the test failed");
-    }
+    report_error(|| f(&env));
 }
 
 pub(crate) struct TestDatabase {
@@ -42,5 +34,47 @@ impl TestDatabase {
 
     pub(crate) fn fake_release(&self) -> fakes::FakeRelease {
         fakes::FakeRelease::new(self)
+    }
+}
+
+pub(crate) fn with_frontend(db: &TestDatabase, f: impl FnOnce(&TestFrontend) -> Result<(), Error>) {
+    let frontend = TestFrontend::new(db);
+    let result = f(&frontend);
+    frontend.server.leak();
+    report_error(|| result)
+}
+
+pub(crate) struct TestFrontend {
+    server: Server,
+    client: Client,
+}
+
+impl TestFrontend {
+    fn new(db: &TestDatabase) -> Self {
+        Self {
+            server: Server::start_test(db.conn.clone()),
+            client: Client::new(),
+        }
+    }
+
+    fn build_request(&self, method: Method, url: &str) -> RequestBuilder {
+        self.client.request(method, &format!("http://{}{}", self.server.addr(), url))
+    }
+
+    pub(crate) fn get(&self, url: &str) -> RequestBuilder {
+        self.build_request(Method::GET, url)
+    }
+}
+
+fn report_error(f: impl FnOnce() -> Result<(), Error>) {
+    if let Err(err) = f() {
+        eprintln!("the test failed: {}", err);
+        for cause in err.iter_causes() {
+            eprintln!("  caused by: {}", cause);
+        }
+
+        eprintln!("{}", err.backtrace());
+
+        panic!("the test failed");
     }
 }
