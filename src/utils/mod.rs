e49aa3945a77d0b285287d91076c82eddfdaee75
Joshua Nelson <jyn514@gmail.com>
2020-01-08 21:04:45 -0500

    make utils pub(crate) where possible

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,12 +1,12 @@
 //! Various utilities for cratesfyi
 
 
-pub use self::copy::copy_doc_dir;
+pub(crate) use self::copy::copy_doc_dir;
 pub use self::github_updater::github_updater;
 pub use self::release_activity_updater::update_release_activity;
 pub use self::daemon::start_daemon;
-pub use self::rustc_version::{parse_rustc_version, get_current_versions, command_result};
-pub use self::html::extract_head_and_body;
+pub(crate) use self::rustc_version::parse_rustc_version;
+pub(crate) use self::html::extract_head_and_body;
 pub use self::queue::add_crate_to_queue;
 pub(crate) use self::cargo_metadata::{CargoMetadata, Package as MetadataPackage};
 
