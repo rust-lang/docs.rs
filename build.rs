4161d40b66672b69d6fa2638a5d75215e7011657
Michael Howell <michael@notriddle.com>
2020-01-22 19:19:16 +0000

    Move (minified) menu.js to separate file

--- build.rs
+++ build.rs
@@ -2,6 +2,7 @@
 extern crate time;
 extern crate sass_rs;
 extern crate git2;
+extern crate minifier;
 
 use std::env;
 use std::path::Path;
@@ -13,6 +14,7 @@ use git2::Repository;
 fn main() {
     write_git_version();
     compile_sass();
+    minify_js();
 }
 
 
@@ -48,4 +50,21 @@ fn compile_sass() {
     let dest_path = Path::new(&env::var("OUT_DIR").unwrap()).join("style.css");
     let mut file = File::create(&dest_path).unwrap();
     file.write_all(css.as_bytes()).unwrap();
+}
+
+fn minify_js() {
+    use minifier::js::minify;
+    use std::io::Read;
+    let mut javascript = String::new();
+    let source_path = Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/templates/menu.js"));
+    let dest_path = Path::new(&env::var("OUT_DIR").unwrap()).join("menu.js");
+    File::open(&source_path)
+        .expect("open templates/menu.js")
+        .read_to_string(&mut javascript)
+        .expect("read templates/menu.js");
+    let javascript = minify(&javascript);
+    File::create(&dest_path)
+        .expect("create target/.../menu.js")
+        .write_all(javascript.as_bytes())
+        .expect("write target/.../menu.js");
 }
--- src/web/routes.rs
+++ src/web/routes.rs
@@ -8,6 +8,7 @@ pub(super) fn build_routes() -> Routes {
     let mut routes = Routes::new();
 
     routes.static_resource("/style.css", super::style_css_handler);
+    routes.static_resource("/menu.js", super::menu_js_handler);
     routes.static_resource("/robots.txt", super::sitemap::robots_txt_handler);
     routes.static_resource("/sitemap.xml", super::sitemap::sitemap_handler);
     routes.static_resource("/opensearch.xml", super::opensearch_xml_handler);
