69b36c43b5dfb5c096578f0610606fa9069d65f2
Pietro Albini <pietro@pietroalbini.org>
2020-01-23 10:26:35 +0100

    wip

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -1,8 +1,29 @@
+mod database;
+mod s3;
+
+use self::database::DatabaseBackend;
+use self::s3::S3Backend;
+use failure::Error;
 use time::Timespec;
 
+#[derive(Debug, PartialEq, Eq)]
 pub(crate) struct Blob {
     pub(crate) path: String,
     pub(crate) mime: String,
     pub(crate) date_updated: Timespec,
     pub(crate) content: Vec<u8>,
+}
+
+pub(crate) enum Storage<'a> {
+    Database(DatabaseBackend<'a>),
+    S3(S3Backend<'a>),
+}
+
+impl Storage<'_> {
+    pub(crate) fn get(&self, path: &str) -> Result<Blob, Error> {
+        match self {
+            Self::Database(db) => db.get(path),
+            Self::S3(s3) => s3.get(path),
+        }
+    }
 }
