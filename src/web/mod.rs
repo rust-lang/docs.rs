56549237e09cf16edbd18218a7af2abbcab32eb3
Joshua Nelson <joshua@yottadb.com>
2020-03-30 13:28:31 -0400

    Revert "Remove dependencies from #461"
    This reverts commit 16f441ccdff76c89f878cf73f673c72fe5580238.

--- src/web/mod.rs
+++ src/web/mod.rs
@@ -302,6 +302,7 @@ fn render_markdown(text: &str) -> String {
 
     let options = {
         let mut options = ComrakOptions::default();
+        options.safe = true;
         options.ext_superscript = true;
         options.ext_table = true;
         options.ext_autolink = true;
