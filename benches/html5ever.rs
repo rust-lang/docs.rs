1b101255d84604a5598026e3faa604ef6542185a
Joshua Nelson <jyn514@gmail.com>
2020-03-31 20:03:37 -0400

    Benchmark HTML parsing
    See https://github.com/rust-lang/docs.rs/pull/671,
    https://github.com/servo/html5ever/issues/414 for rationale.
    
    This makes the `extract_head_and_body` function public since I couldn't
    find a cfg flag that would do so only during tests.

---
+++ benches/html5ever.rs
@@ -0,0 +1,13 @@
+extern crate criterion;
+extern crate cratesfyi;
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use cratesfyi::utils::extract_head_and_body;
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let html = std::fs::read_to_string("benches/struct.CaptureMatches.html").unwrap();
+    c.bench_function("parse regex html", |b| b.iter(|| extract_head_and_body(black_box(&html))));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
