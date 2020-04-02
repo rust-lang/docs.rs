2ff339cc8a553ebaf464be0aaf27d4afeb1a19c4
Chase Wilson <contact@chasewilson.dev>
2020-04-02 14:14:50 -0500

    Focus, one thing at a time

--- src/web/crate_details.rs
+++ src/web/crate_details.rs
@@ -146,7 +146,7 @@ impl CrateDetails {
 
         let rows = conn.query(query, &[&name, &version]).unwrap();
 
-        if rows.is_empty() {
+        if rows.len() == 0 {
             return None;
         }
 
