4549ed89b5c4e7579ea038457edf2243bb7367f2
Joshua Nelson <jyn514@gmail.com>
2020-04-15 10:34:19 -0400

    Use `ON CONFLICT` instead of rewriting it manually

--- src/storage/database.rs
+++ src/storage/database.rs
@@ -35,23 +35,13 @@ impl<'a> DatabaseBackend<'a> {
     }
     pub(super) fn store_batch(&self, batch: &[Blob], trans: &Transaction) -> Result<(), Error> {
         for blob in batch {
-            // check if file already exists in database
-            let rows = self
-                .conn
-                .query("SELECT COUNT(*) FROM files WHERE path = $1", &[&blob.path])?;
-
-            if rows.get(0).get::<usize, i64>(0) == 0 {
-                trans.query(
-                    "INSERT INTO files (path, mime, content) VALUES ($1, $2, $3)",
-                    &[&blob.path, &blob.mime, &blob.content],
-                )?;
-            } else {
-                trans.query(
-                    "UPDATE files SET mime = $2, content = $3, date_updated = NOW() \
-                                WHERE path = $1",
-                    &[&blob.path, &blob.mime, &blob.content],
-                )?;
-            }
+            trans.query(
+                "INSERT INTO files (path, mime, content)
+                 VALUES ($1, $2, $3)
+                 ON CONFLICT (path) DO UPDATE
+                    SET mime = EXCLUDED.mime, content = EXCLUDED.content",
+                &[&blob.path, &blob.mime, &blob.content],
+            )?;
         }
         Ok(())
     }
