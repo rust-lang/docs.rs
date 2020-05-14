81b98a49db38435d986d78809013d2465c155d2d
Chase Wilson <me@chasewilson.dev>
2020-05-14 09:25:09 -0500

    Upgraded reqwest

--- src/utils/pubsubhubbub.rs
+++ src/utils/pubsubhubbub.rs
@@ -1,6 +1,9 @@
 use std::collections::HashMap;
 
-use reqwest::*;
+use reqwest::{
+    blocking::{Client, Response},
+    Result,
+};
 
 fn ping_hub(url: &str) -> Result<Response> {
     let mut params = HashMap::new();
