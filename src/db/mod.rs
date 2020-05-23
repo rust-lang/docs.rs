6a5d9633a728fb1ad7aa63cb38bb848b11d6d03a
Wim Looman <git@nemo157.com>
2020-05-23 20:54:07 +0200

    Don't require the db connection pool to sit at maximum when idle

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -28,19 +28,27 @@ pub fn connect_db() -> Result<Connection, failure::Error> {
 pub(crate) fn create_pool() -> r2d2::Pool<r2d2_postgres::PostgresConnectionManager> {
     let db_url = env::var("CRATESFYI_DATABASE_URL")
         .expect("CRATESFYI_DATABASE_URL environment variable is not exists");
-    let pool_size = env::var("DOCSRS_POOL_SIZE")
+    let max_pool_size = env::var("DOCSRS_MAX_POOL_SIZE")
         .map(|s| {
             s.parse::<u32>()
-                .expect("DOCSRS_POOL_SIZE must be an integer")
+                .expect("DOCSRS_MAX_POOL_SIZE must be an integer")
         })
         .unwrap_or(90);
+
+    let min_pool_idle = env::var("DOCSRS_MIN_POOL_IDLE")
+        .map(|s| {
+            s.parse::<u32>()
+                .expect("DOCSRS_MIN_POOL_IDLE must be an integer")
+        })
+        .unwrap_or(10);
 
     let manager =
         r2d2_postgres::PostgresConnectionManager::new(&db_url[..], r2d2_postgres::TlsMode::None)
             .expect("Failed to create PostgresConnectionManager");
 
     r2d2::Pool::builder()
-        .max_size(pool_size)
+        .max_size(max_pool_size)
+        .min_idle(Some(min_pool_idle))
         .build(manager)
         .expect("Failed to create r2d2 pool")
 }
