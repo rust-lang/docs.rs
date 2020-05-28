1f14085e87a5e181e29b1a495837320d20d15772
Joshua Nelson <jyn514@gmail.com>
2020-05-27 22:38:59 -0400

    Add benchmark for compression/decompression

---
+++ benches/compression.rs
@@ -0,0 +1,19 @@
+use cratesfyi::storage::{compress, decompress};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    // this isn't a great benchmark because it only tests on one file
+    // ideally we would build a whole crate and compress each file, taking the average
+    let html = std::fs::read_to_string("benches/struct.CaptureMatches.html").unwrap();
+    let html_slice = html.as_bytes();
+    c.bench_function("compress regex html", |b| {
+        b.iter(|| compress(black_box(html_slice)))
+    });
+    let compressed = compress(html_slice).unwrap();
+    c.bench_function("decompress regex html", |b| {
+        b.iter(|| decompress(black_box(compressed.as_slice())))
+    });
+}
+
+criterion_group!(compression, criterion_benchmark);
+criterion_main!(compression);
