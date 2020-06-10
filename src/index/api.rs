5bb7e6ca74dbeb7cb8e52c05fc0efc37b818a56e
Wim Looman <git@nemo157.com>
2020-06-10 18:19:49 +0200

    Rewrite index API code and fill in defaults for any failure

--- src/index/api.rs
+++ src/index/api.rs
@@ -1,10 +1,9 @@
-use std::io::Read;
-
 use chrono::{DateTime, Utc};
 use failure::err_msg;
+use log::info;
 use reqwest::header::{HeaderValue, ACCEPT, USER_AGENT};
 use semver::Version;
-use serde_json::Value;
+use serde::Deserialize;
 use url::Url;
 
 use crate::error::Result;
@@ -16,7 +15,7 @@ const APP_USER_AGENT: &str = concat!(
 );
 
 pub(crate) struct Api {
-    api_base: Url,
+    api_base: Option<Url>,
     client: reqwest::blocking::Client,
 }
 
@@ -35,7 +34,7 @@ pub(crate) struct CrateOwner {
 }
 
 impl Api {
-    pub(super) fn new(api_base: &Url) -> Result<Self> {
+    pub(super) fn new(api_base: Option<Url>) -> Result<Self> {
         let headers = vec![
             (USER_AGENT, HeaderValue::from_static(APP_USER_AGENT)),
             (ACCEPT, HeaderValue::from_static("application/json")),
@@ -47,23 +46,34 @@ impl Api {
             .default_headers(headers)
             .build()?;
 
-        Ok(Self {
-            api_base: api_base.clone(),
-            client,
-        })
+        Ok(Self { api_base, client })
     }
 
-    pub(crate) fn get_crate_data(&self, name: &str, version: &str) -> Result<RegistryCrateData> {
-        let (release_time, yanked, downloads) =
-            self.get_release_time_yanked_downloads(name, version)?;
-        let owners = self.get_owners(name)?;
+    fn api_base(&self) -> Result<Url> {
+        self.api_base
+            .clone()
+            .ok_or_else(|| err_msg("index is missing an api base url"))
+    }
 
-        Ok(RegistryCrateData {
+    pub(crate) fn get_crate_data(&self, name: &str, version: &str) -> RegistryCrateData {
+        let (release_time, yanked, downloads) = self
+            .get_release_time_yanked_downloads(name, version)
+            .unwrap_or_else(|err| {
+                info!("Failed to get crate data for {}-{}: {}", name, version, err);
+                (Utc::now(), false, 0)
+            });
+
+        let owners = self.get_owners(name).unwrap_or_else(|err| {
+            info!("Failed to get owners for {}-{}: {}", name, version, err);
+            Vec::new()
+        });
+
+        RegistryCrateData {
             release_time,
             yanked,
             downloads,
             owners,
-        })
+        }
     }
 
     /// Get release_time, yanked and downloads from the registry's API
@@ -73,126 +83,81 @@ impl Api {
         version: &str,
     ) -> Result<(DateTime<Utc>, bool, i32)> {
         let url = {
-            let mut url = self.api_base.clone();
+            let mut url = self.api_base()?;
             url.path_segments_mut()
                 .map_err(|()| err_msg("Invalid API url"))?
                 .extend(&["api", "v1", "crates", name, "versions"]);
             url
         };
-        // FIXME: There is probably better way to do this
-        //        and so many unwraps...
-        // TODO: When reqwest is upgraded remove the `as_str` here
-        let mut res = self.client.get(url.as_str()).send()?;
-        let mut body = String::new();
-        res.read_to_string(&mut body).unwrap();
-        let json: Value = serde_json::from_str(&body[..])?;
-        let versions = json
-            .as_object()
-            .and_then(|o| o.get("versions"))
-            .and_then(|v| v.as_array())
-            .ok_or_else(|| err_msg("Not a JSON object"))?;
 
-        let (mut release_time, mut yanked, mut downloads) = (None, None, None);
-
-        for version_data in versions {
-            let version_data = version_data
-                .as_object()
-                .ok_or_else(|| err_msg("Not a JSON object"))?;
-            let version_num = version_data
-                .get("num")
-                .and_then(|v| v.as_str())
-                .ok_or_else(|| err_msg("Not a JSON object"))?;
-
-            if Version::parse(version_num)?.to_string() == version {
-                let release_time_raw = version_data
-                    .get("created_at")
-                    .and_then(|c| c.as_str())
-                    .ok_or_else(|| err_msg("Not a JSON object"))?;
-
-                release_time = Some(
-                    DateTime::parse_from_str(release_time_raw, "%Y-%m-%dT%H:%M:%S%.f%:z")?
-                        .with_timezone(&Utc),
-                );
-
-                yanked = Some(
-                    version_data
-                        .get("yanked")
-                        .and_then(|c| c.as_bool())
-                        .ok_or_else(|| err_msg("Not a JSON object"))?,
-                );
-
-                downloads = Some(
-                    version_data
-                        .get("downloads")
-                        .and_then(|c| c.as_i64())
-                        .ok_or_else(|| err_msg("Not a JSON object"))? as i32,
-                );
-
-                break;
-            }
+        #[derive(Deserialize)]
+        struct Response {
+            versions: Vec<VersionData>,
         }
 
-        Ok((
-            release_time.unwrap_or_else(Utc::now),
-            yanked.unwrap_or(false),
-            downloads.unwrap_or(0),
-        ))
+        #[derive(Deserialize)]
+        struct VersionData {
+            num: Version,
+            #[serde(default = "Utc::now")]
+            created_at: DateTime<Utc>,
+            #[serde(default)]
+            yanked: bool,
+            #[serde(default)]
+            downloads: i32,
+        }
+
+        let response: Response = self.client.get(url).send()?.error_for_status()?.json()?;
+
+        let version = Version::parse(version)?;
+        let version = response
+            .versions
+            .into_iter()
+            .find(|data| data.num == version)
+            .ok_or_else(|| err_msg("Could not find version in response"))?;
+
+        Ok((version.created_at, version.yanked, version.downloads))
     }
 
     /// Fetch owners from the registry's API
     fn get_owners(&self, name: &str) -> Result<Vec<CrateOwner>> {
         let url = {
-            let mut url = self.api_base.clone();
+            let mut url = self.api_base()?;
             url.path_segments_mut()
                 .map_err(|()| err_msg("Invalid API url"))?
                 .extend(&["api", "v1", "crates", name, "owners"]);
             url
         };
-        // TODO: When reqwest is upgraded remove the `as_str` here
-        let mut res = self.client.get(url.as_str()).send()?;
-        // FIXME: There is probably better way to do this
-        //        and so many unwraps...
-        let mut body = String::new();
-        res.read_to_string(&mut body).unwrap();
-        let json: Value = serde_json::from_str(&body[..])?;
 
-        let owners = json
-            .as_object()
-            .and_then(|j| j.get("users"))
-            .and_then(|j| j.as_array());
+        #[derive(Deserialize)]
+        struct Response {
+            users: Vec<OwnerData>,
+        }
 
-        let result = if let Some(owners) = owners {
-            owners
-                .iter()
-                .filter_map(|owner| {
-                    fn extract<'a>(owner: &'a Value, field: &str) -> &'a str {
-                        owner
-                            .as_object()
-                            .and_then(|o| o.get(field))
-                            .and_then(|o| o.as_str())
-                            .unwrap_or_default()
-                    }
+        #[derive(Deserialize)]
+        struct OwnerData {
+            #[serde(default)]
+            avatar: String,
+            #[serde(default)]
+            email: String,
+            #[serde(default)]
+            login: String,
+            #[serde(default)]
+            name: String,
+        }
 
-                    let avatar = extract(owner, "avatar");
-                    let email = extract(owner, "email");
-                    let login = extract(owner, "login");
-                    let name = extract(owner, "name");
+        let response: Response = self.client.get(url).send()?.error_for_status()?.json()?;
 
-                    if login.is_empty() {
-                        return None;
-                    }
-
-                    Some(CrateOwner {
-                        avatar: avatar.to_string(),
-                        email: email.to_string(),
-                        login: login.to_string(),
-                        name: name.to_string(),
-                    })
-                })
-                .collect()
-        } else {
-            Vec::new()
-        };
+        let result = response
+            .users
+            .into_iter()
+            .filter(|data| !data.login.is_empty())
+            .map(|data| CrateOwner {
+                avatar: data.avatar,
+                email: data.email,
+                login: data.login,
+                name: data.name,
+            })
+            .collect();
 
         Ok(result)
     }
