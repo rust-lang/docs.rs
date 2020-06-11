rust-lang/docs.rs

commit 4dc57befdf4e11f6248f39f52264722146cec6ef

Reverted file sorting

src/web/source.rs
@@ -104,48 +104,49 @@ impl FileList {
 
         let files: Value = rows.get(0).get_opt(5).unwrap().ok()?;
 
+        let mut file_list = Vec::new();
         if let Some(files) = files.as_array() {
-            let mut file_list: Vec<File> = files
-                .iter()
-                .filter_map(|file| {
-                    if let Some(file) = file.as_array() {
-                        let mime = file[0].as_str().unwrap();
-                        let path = file[1].as_str().unwrap();
+            file_list.reserve(files.len());
 
-                        // skip .cargo-ok generated by cargo
-                        if path == ".cargo-ok" {
-                            return None;
-                        }
+            for file in files {
+                if let Some(file) = file.as_array() {
+                    let mime = file[0].as_str().unwrap();
+                    let path = file[1].as_str().unwrap();
 
-                        // look only files for req_path
-                        if path.starts_with(&req_path) {
-                            // remove req_path from path to reach files in this directory
-                            let path = path.replace(&req_path, "");
-                            let path_splited: Vec<&str> = path.split('/').collect();
-
-                            // if path have '/' it is a directory
-                            let ftype = if path_splited.len() > 1 {
-                                FileType::Dir
-                            } else if mime.starts_with("text") && path_splited[0].ends_with(".rs") {
-                                FileType::RustSource
-                            } else if mime.starts_with("text") {
-                                FileType::Text
-                            } else {
-                                FileType::Binary
-                            };
-
-                            let file = File {
-                                name: path_splited[0].to_owned(),
-                                file_type: ftype,
-                            };
-
-                            return Some(file);
-                        }
+                    // skip .cargo-ok generated by cargo
+                    if path == ".cargo-ok" {
+                        return None;
                     }
 
-                    None
-                })
-                .collect::<Vec<_>>();
+                    // look only files for req_path
+                    if path.starts_with(&req_path) {
+                        // remove req_path from path to reach files in this directory
+                        let path = path.replace(&req_path, "");
+                        let path_splited: Vec<&str> = path.split('/').collect();
+
+                        // if path have '/' it is a directory
+                        let ftype = if path_splited.len() > 1 {
+                            FileType::Dir
+                        } else if mime.starts_with("text") && path_splited[0].ends_with(".rs") {
+                            FileType::RustSource
+                        } else if mime.starts_with("text") {
+                            FileType::Text
+                        } else {
+                            FileType::Binary
+                        };
+
+                        let file = File {
+                            name: path_splited[0].to_owned(),
+                            file_type: ftype,
+                        };
+
+                        // avoid adding duplicates, a directory may occur more than once
+                        if !file_list.contains(&file) {
+                            file_list.push(file);
+                        }
+                    }
+                }
+            }
 
             if file_list.is_empty() {
                 return None;
@@ -161,9 +162,6 @@ impl FileList {
                     a.name.to_lowercase().cmp(&b.name.to_lowercase())
                 }
             });
-
-            // avoid adding duplicates, a directory may occur more than once
-            file_list.dedup();
 
             Some(FileList {
                 metadata: MetaData {

commit b1e4deca84c295e9d36ea4a6389c5ca0b6a5a996

Don't return 404 if .cargo-ok file exists
This was a regression introduced in https://github.com/rust-lang/docs.rs/pull/692.

src/web/source.rs
@@ -115,7 +115,7 @@ impl FileList {
 
                     // skip .cargo-ok generated by cargo
                     if path == ".cargo-ok" {
-                        return None;
+                        continue;
                     }
 
                     // look only files for req_path
@@ -261,6 +261,7 @@ pub fn source_browser_handler(req: &mut Request) -> IronResult<Response> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::test::{assert_success, wrapper};
     use serde_json::json;
 
     #[test]
@@ -305,5 +306,21 @@ mod tests {
         });
 
         assert_eq!(correct_json, serde_json::to_value(&file_list).unwrap(),);
+    }
+
+    #[test]
+    fn cargo_ok_not_skipped() {
+        wrapper(|env| {
+            let db = env.db();
+            db.fake_release()
+                .name("fake")
+                .version("0.1.0")
+                .source_file(".cargo-ok", b"ok")
+                .source_file("README.md", b"hello")
+                .create()?;
+            let web = env.frontend();
+            assert_success("/crate/fake/0.1.0/source/", web)?;
+            Ok(())
+        });
     }
 }