e8646157147bc0075db753b68b3ffbaa54bfb25c
Pietro Albini <pietro@pietroalbini.org>
2020-06-12 17:17:34 +0200

    sized_buffer: add buffer that limits the size of its content

--- src/error.rs
+++ src/error.rs
@@ -5,3 +5,14 @@ use std::result::Result as StdResult;
 pub(crate) use failure::Error;
 
 pub type Result<T> = StdResult<T, Error>;
+
+#[derive(Debug, Copy, Clone)]
+pub(crate) struct SizeLimitReached;
+
+impl std::error::Error for SizeLimitReached {}
+
+impl std::fmt::Display for SizeLimitReached {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "the size limit for the buffer was reached")
+    }
+}
---
+++ src/utils/sized_buffer.rs
@@ -0,0 +1,72 @@
+use std::io::{Error as IoError, ErrorKind, Write};
+
+pub(crate) struct SizedBuffer {
+    inner: Vec<u8>,
+    limit: usize,
+}
+
+impl SizedBuffer {
+    pub(crate) fn new(limit: usize) -> Self {
+        SizedBuffer {
+            inner: Vec::new(),
+            limit,
+        }
+    }
+
+    pub(crate) fn reserve(&mut self, amount: usize) {
+        if self.inner.len() + amount > self.limit {
+            self.inner.reserve_exact(self.limit - self.inner.len());
+        } else {
+            self.inner.reserve_exact(amount);
+        }
+    }
+
+    pub(crate) fn into_inner(self) -> Vec<u8> {
+        self.inner
+    }
+}
+
+impl Write for SizedBuffer {
+    fn write(&mut self, buf: &[u8]) -> Result<usize, IoError> {
+        if self.inner.len() + buf.len() > self.limit {
+            Err(IoError::new(
+                ErrorKind::Other,
+                crate::error::SizeLimitReached,
+            ))
+        } else {
+            self.inner.write(buf)
+        }
+    }
+
+    fn flush(&mut self) -> Result<(), IoError> {
+        self.inner.flush()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sized_buffer() {
+        let mut buffer = SizedBuffer::new(1024);
+
+        // Add two chunks of 500 bytes
+        assert_eq!(500, buffer.write(&[0; 500]).unwrap());
+        assert_eq!(500, buffer.write(&[0; 500]).unwrap());
+
+        // Ensure adding a third chunk fails
+        let error = buffer.write(&[0; 500]).unwrap_err();
+        assert!(error
+            .get_ref()
+            .unwrap()
+            .is::<crate::error::SizeLimitReached>());
+
+        // Ensure all the third chunk was discarded
+        assert_eq!(1000, buffer.inner.len());
+
+        // Ensure it's possible to reach the limit
+        assert_eq!(24, buffer.write(&[0; 24]).unwrap());
+        assert_eq!(1024, buffer.inner.len());
+    }
+}
