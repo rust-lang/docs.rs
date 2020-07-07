9e415bbf631ac39df02f6a4abe4bcc4124eae440
Joshua Nelson <joshua@yottadb.com>
2020-07-07 17:31:23 -0400

    Revert "Source"
    This reverts commit 754b91df878071b6f13907b40b89745b59542917.

--- src/web/source.rs
+++ src/web/source.rs
@@ -1,32 +1,67 @@
 //! Source code browser
 
-use crate::{
-    db::Pool,
-    impl_webpage,
-    web::{error::Nope, file::File as DbFile, page::WebPage, MetaData},
-    Config,
-};
-use iron::{status::Status, IronError, IronResult, Request, Response};
+use super::file::File as DbFile;
+use super::page::Page;
+use super::MetaData;
+use crate::db::Pool;
+use crate::Config;
+use iron::prelude::*;
 use postgres::Connection;
 use router::Router;
-use serde::Serialize;
+use serde::ser::{Serialize, SerializeStruct, Serializer};
 use serde_json::Value;
 use std::cmp::Ordering;
+use std::collections::HashMap;
 
-/// A source file's name and mime type
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Serialize)]
+/// A source file's type
+#[derive(PartialEq, PartialOrd)]
+enum FileType {
+    Dir,
+    Text,
+    Binary,
+    RustSource,
+}
+
+/// A source file
+#[derive(PartialEq, PartialOrd)]
 struct File {
-    /// The name of the file
     name: String,
-    /// The mime type of the file
-    mime: String,
+    file_type: FileType,
 }
 
 /// A list of source files
-#[derive(Debug, Clone, PartialEq, Serialize)]
 struct FileList {
     metadata: MetaData,
     files: Vec<File>,
+}
+
+impl Serialize for FileList {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut state = serializer.serialize_struct("FileList", 2)?;
+        state.serialize_field("metadata", &self.metadata)?;
+
+        let mut files = Vec::with_capacity(self.files.len());
+        for file in &self.files {
+            let mut map = HashMap::with_capacity(2);
+            map.insert("name", Value::String(file.name.to_owned()));
+
+            let file_type = match file.file_type {
+                FileType::Dir => "file_type_dir",
+                FileType::Text => "file_type_text",
+                FileType::Binary => "file_type_binary",
+                FileType::RustSource => "file_type_rust_source",
+            };
+            map.insert(file_type, Value::Bool(true));
+
+            files.push(map);
+        }
+        state.serialize_field("files", &files)?;
+
+        state.end()
+    }
 }
 
 impl FileList {
@@ -91,15 +126,19 @@ impl FileList {
                         let path_splited: Vec<&str> = path.split('/').collect();
 
                         // if path have '/' it is a directory
-                        let mime = if path_splited.len() > 1 {
-                            "dir".to_owned()
+                        let ftype = if path_splited.len() > 1 {
+                            FileType::Dir
+                        } else if mime.starts_with("text") && path_splited[0].ends_with(".rs") {
+                            FileType::RustSource
+                        } else if mime.starts_with("text") {
+                            FileType::Text
                         } else {
-                            mime.to_owned()
+                            FileType::Binary
                         };
 
                         let file = File {
                             name: path_splited[0].to_owned(),
-                            mime,
+                            file_type: ftype,
                         };
 
                         // avoid adding duplicates, a directory may occur more than once
@@ -116,9 +155,9 @@ impl FileList {
 
             file_list.sort_by(|a, b| {
                 // directories must be listed first
-                if a.mime == "dir" && b.mime != "dir" {
+                if a.file_type == FileType::Dir && b.file_type != FileType::Dir {
                     Ordering::Less
-                } else if a.mime != "dir" && b.mime == "dir" {
+                } else if a.file_type != FileType::Dir && b.file_type == FileType::Dir {
                     Ordering::Greater
                 } else {
                     a.name.to_lowercase().cmp(&b.name.to_lowercase())
@@ -140,18 +179,6 @@ impl FileList {
             None
         }
     }
-}
-
-#[derive(Debug, Clone, PartialEq, Serialize)]
-struct SourcePage {
-    file_list: FileList,
-    show_parent_link: bool,
-    file_content: Option<String>,
-    is_rust_source: bool,
-}
-
-impl_webpage! {
-    SourcePage = "crate/source.html",
 }
 
 pub fn source_browser_handler(req: &mut Request) -> IronResult<Response> {
@@ -195,7 +222,7 @@ pub fn source_browser_handler(req: &mut Request) -> IronResult<Response> {
         None
     };
 
-    let (file_content, is_rust_source) = if let Some(file) = file {
+    let (content, is_rust_source) = if let Some(file) = file {
         // serve the file with DatabaseFileHandler if file isn't text and not empty
         if !file.0.mime.starts_with("text") && !file.is_empty() {
             return Ok(file.serve());
@@ -211,21 +238,77 @@ pub fn source_browser_handler(req: &mut Request) -> IronResult<Response> {
         (None, false)
     };
 
-    let file_list = FileList::from_path(&conn, &name, &version, &req_path)
-        .ok_or_else(|| IronError::new(Nope::NoResults, Status::NotFound))?;
-
-    SourcePage {
-        file_list,
-        show_parent_link: !req_path.is_empty(),
-        file_content,
-        is_rust_source,
+    let list = FileList::from_path(&conn, &name, &version, &req_path);
+    if list.is_none() {
+        use super::error::Nope;
+        use iron::status;
+        return Err(IronError::new(Nope::NoResults, status::NotFound));
     }
-    .into_response(req)
+
+    let page = Page::new(list)
+        .set_bool("show_parent_link", !req_path.is_empty())
+        .set_true("javascript_highlightjs")
+        .set_true("show_package_navigation")
+        .set_true("package_source_tab");
+
+    if let Some(content) = content {
+        page.set("file_content", &content)
+            .set_bool("file_content_rust_source", is_rust_source)
+            .to_resp("source")
+    } else {
+        page.to_resp("source")
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     use crate::test::{assert_success, wrapper};
+    use serde_json::json;
+
+    #[test]
+    fn serialize_file_list() {
+        let file_list = FileList {
+            metadata: MetaData {
+                name: "rcc".to_string(),
+                version: "0.0.0".to_string(),
+                description: Some("it compiles an unholy language".to_string()),
+                target_name: None,
+                rustdoc_status: true,
+                default_target: "x86_64-unknown-linux-gnu".to_string(),
+            },
+            files: vec![
+                File {
+                    name: "main.rs".to_string(),
+                    file_type: FileType::RustSource,
+                },
+                File {
+                    name: "lib.rs".to_string(),
+                    file_type: FileType::RustSource,
+                },
+            ],
+        };
+
+        let correct_json = json!({
+            "metadata": {
+                "name": "rcc",
+                "version": "0.0.0",
+                "description": "it compiles an unholy language",
+                "target_name": null,
+                "rustdoc_status": true,
+                "default_target": "x86_64-unknown-linux-gnu"
+            },
+            "files": [{
+                "name": "main.rs",
+                "file_type_rust_source": true
+            }, {
+                "name": "lib.rs",
+                "file_type_rust_source": true
+            }],
+        });
+
+        assert_eq!(correct_json, serde_json::to_value(&file_list).unwrap(),);
+    }
 
     #[test]
     fn cargo_ok_not_skipped() {
