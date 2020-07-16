d58aa7da127f36138cc6f5a18b1f45dc9f7cc98c
Joshua Nelson <jyn514@gmail.com>
2020-07-15 21:36:47 -0400

    delete_crate.rs -> delete.rs

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -2,7 +2,7 @@
 
 pub(crate) use self::add_package::add_build_into_database;
 pub(crate) use self::add_package::add_package_into_database;
-pub use self::delete_crate::{delete_crate, delete_version};
+pub use self::delete::{delete_crate, delete_version};
 pub use self::file::add_path_into_database;
 pub use self::migrate::migrate;
 pub use self::pool::{Pool, PoolError};
@@ -12,7 +12,7 @@ pub(crate) use self::pool::PoolConnection;
 
 mod add_package;
 pub mod blacklist;
-mod delete_crate;
+mod delete;
 pub(crate) mod file;
 mod migrate;
 mod pool;
