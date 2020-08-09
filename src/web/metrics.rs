13e68cafa3c426b1636d101e9aca72248db7160a
Pietro Albini <pietro@pietroalbini.org>
2020-08-09 13:09:11 +0200

    web: merge metrics tests

--- src/web/metrics.rs
+++ src/web/metrics.rs
@@ -108,80 +108,32 @@ impl Drop for RenderingTimesRecorder<'_> {
 #[cfg(test)]
 mod tests {
     use crate::test::{assert_success, wrapper};
+    use std::collections::HashMap;
 
     #[test]
-    fn home_page() {
-        wrapper(|env| {
-            let frontend = env.frontend();
-            let metrics = env.metrics();
+    fn test_response_times_count_being_collected() {
+        const ROUTES: &[(&str, &str)] = &[
+            ("", "/"),
+            ("/", "/"),
+            ("/crate/hexponent/0.2.0", "/crate/:name/:version"),
+            ("/crate/rcc/0.0.0", "/crate/:name/:version"),
+            ("/index.js", "static resource"),
+            ("/menu.js", "static resource"),
+            ("/opensearch.xml", "static resource"),
+            ("/releases", "/releases"),
+            ("/releases/feed", "static resource"),
+            ("/releases/queue", "/releases/queue"),
+            ("/releases/recent-failures", "/releases/recent-failures"),
+            (
+                "/releases/recent-failures/1",
+                "/releases/recent-failures/:page",
+            ),
+            ("/releases/recent/1", "/releases/recent/:page"),
+            ("/robots.txt", "static resource"),
+            ("/sitemap.xml", "static resource"),
+            ("/style.css", "static resource"),
+        ];
 
-            frontend.get("/").send()?;
-            frontend.get("/").send()?;
-            assert_eq!(metrics.routes_visited.with_label_values(&["/"]).get(), 2);
-            assert_eq!(
-                metrics
-                    .response_time
-                    .with_label_values(&["/"])
-                    .get_sample_count(),
-                2
-            );
-
-            frontend.get("").send()?;
-            frontend.get("").send()?;
-            assert_eq!(metrics.routes_visited.with_label_values(&["/"]).get(), 4);
-            assert_eq!(
-                metrics
-                    .response_time
-                    .with_label_values(&["/"])
-                    .get_sample_count(),
-                4
-            );
-
-            Ok(())
-        })
-    }
-
-    #[test]
-    fn resources() {
-        wrapper(|env| {
-            let frontend = env.frontend();
-            let metrics = env.metrics();
-
-            let routes = [
-                "/style.css",
-                "/index.js",
-                "/menu.js",
-                "/sitemap.xml",
-                "/opensearch.xml",
-                "/robots.txt",
-            ];
-
-            for route in routes.iter() {
-                frontend.get(route).send()?;
-                frontend.get(route).send()?;
-            }
-
-            assert_eq!(
-                metrics
-                    .routes_visited
-                    .with_label_values(&["static resource"])
-                    .get(),
-                12
-            );
-            assert_eq!(
-                metrics
-                    .response_time
-                    .with_label_values(&["static resource"])
-                    .get_sample_count(),
-                12
-            );
-
-            Ok(())
-        })
-    }
-
-    #[test]
-    fn releases() {
         wrapper(|env| {
             env.fake_release()
                 .name("rcc")
@@ -193,47 +145,6 @@ mod tests {
                 .version("1.0.0")
                 .build_result_successful(false)
                 .create()?;
-
-            let frontend = env.frontend();
-            let metrics = env.metrics();
-
-            let routes = [
-                ("/releases", "/releases"),
-                ("/releases/recent/1", "/releases/recent/:page"),
-                ("/releases/feed", "static resource"),
-                ("/releases/queue", "/releases/queue"),
-                ("/releases/recent-failures", "/releases/recent-failures"),
-                (
-                    "/releases/recent-failures/1",
-                    "/releases/recent-failures/:page",
-                ),
-            ];
-
-            for (route, correct) in routes.iter() {
-                frontend.get(route).send()?;
-                frontend.get(route).send()?;
-
-                assert_eq!(
-                    metrics.routes_visited.with_label_values(&[*correct]).get(),
-                    2
-                );
-                assert_eq!(
-                    metrics
-                        .response_time
-                        .with_label_values(&[*correct])
-                        .get_sample_count(),
-                    2
-                );
-            }
-
-            Ok(())
-        })
-    }
-
-    #[test]
-    fn crates() {
-        wrapper(|env| {
-            env.fake_release().name("rcc").version("0.0.0").create()?;
             env.fake_release()
                 .name("hexponent")
                 .version("0.2.0")
@@ -242,34 +153,37 @@ mod tests {
             let frontend = env.frontend();
             let metrics = env.metrics();
 
-            let routes = ["/crate/rcc/0.0.0", "/crate/hexponent/0.2.0"];
-
-            for route in routes.iter() {
+            for (route, _) in ROUTES.iter() {
                 frontend.get(route).send()?;
                 frontend.get(route).send()?;
             }
 
-            assert_eq!(
-                metrics
-                    .routes_visited
-                    .with_label_values(&["/crate/:name/:version"])
-                    .get(),
-                4
-            );
-            assert_eq!(
-                metrics
-                    .response_time
-                    .with_label_values(&["/crate/:name/:version"])
-                    .get_sample_count(),
-                4
-            );
+            let mut expected = HashMap::new();
+            for (_, correct) in ROUTES.iter() {
+                let entry = expected.entry(*correct).or_insert(0);
+                *entry += 2;
+            }
+
+            for (label, count) in expected.iter() {
+                assert_eq!(
+                    metrics.routes_visited.with_label_values(&[*label]).get(),
+                    *count
+                );
+                assert_eq!(
+                    metrics
+                        .response_time
+                        .with_label_values(&[*label])
+                        .get_sample_count(),
+                    *count as u64
+                );
+            }
 
             Ok(())
         })
     }
 
     #[test]
-    fn metrics() {
+    fn test_metrics_page_success() {
         wrapper(|env| {
             let web = env.frontend();
             assert_success("/about/metrics", web)
