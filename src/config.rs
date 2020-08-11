0ab6ece3a1fc5c0b6f65748008739ed7068630e9
ohaddahan <ohaddahan@gmail.com>
2020-08-11 23:22:36 +0300

    Update src/config.rs
    Co-authored-by: Chase Wilson <buckshot1233@gmail.com>

--- src/config.rs
+++ src/config.rs
@@ -30,7 +30,6 @@ pub struct Config {
     pub(crate) max_file_size_html: usize,
     // The most memory that can be used to parse an HTML file
     pub(crate) max_parse_memory: usize,
-
     // Time between 'git gc' calls in seconds
     pub(crate) registry_gc_interval: u64,
 }
