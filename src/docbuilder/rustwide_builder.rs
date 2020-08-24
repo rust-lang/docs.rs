dc65452f718734cb2f54acca3639a5a9e465e3ef
Joshua Nelson <jyn514@gmail.com>
2020-08-24 12:13:40 -0400

    Add -Z unstable-options to metadata library
    Many options passed on docs.rs require -Z unstable-options,
    so passing it in docs.rs and not crater wouldn't completely replicate
    the build environment. As a consequence, this library can only be used
    with nightly builds of rustdoc; this adds documentation saying so.

--- src/docbuilder/rustwide_builder.rs
+++ src/docbuilder/rustwide_builder.rs
@@ -606,7 +606,7 @@ impl RustwideBuilder {
 
         let mut env_vars = metadata.environment_variables();
         let rustdoc_flags = env_vars.entry("RUSTDOCFLAGS").or_default();
-        rustdoc_flags.push_str(" -Z unstable-options --static-root-path / --cap-lints warn ");
+        rustdoc_flags.push_str(" --static-root-path / --cap-lints warn ");
         rustdoc_flags.push_str(&rustdoc_flags_extras.join(" "));
 
         let mut command = build
