00ba0b2e119854a28d9ec293eba2fac660791ef3
Wim Looman <git@nemo157.com>
2020-10-26 21:25:10 +0100

    Detect theme changes on all docs.rs pages
    This allows us to change non-rustdoc pages if another open tab is
    looking at a rustdoc page and changes the theme

--- templates/theme.js
+++ templates/theme.js
@@ -1,7 +1,18 @@
-// This is a global function also called from a script in ./rustdoc/body.html
-// which detects when the rustdoc theme is changed
 function applyTheme(theme) {
   document.documentElement.dataset.theme = theme;
 }
+
+window.addEventListener('storage', function (ev) {
+  if (ev.key === 'rustdoc-theme') {
+    applyTheme(ev.newValue);
+  }
+});
+
+// see ../static/storage-change-detection.html for details
+window.addEventListener('message', function (ev) {
+  if (ev.data && ev.data.storage && ev.data.storage.key === 'rustdoc-theme') {
+    applyTheme(ev.data.storage.value);
+  }
+});
 
 applyTheme(window.localStorage.getItem('rustdoc-theme'));
