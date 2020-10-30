a46ff7fa38d3c84e54ffad5e26b6616ee5215dfb
Joshua Nelson <joshua@yottadb.com>
2020-10-30 11:46:35 -0400

    Revert "Don`t check for deleted CRATE_VERSION"
    This reverts commit dbbb5a19b43e26c82824d46cf0374d59bcef93e3.

--- src/bin/cratesfyi.rs
+++ src/bin/cratesfyi.rs
@@ -262,7 +262,11 @@ enum BuildSubcommand {
         crate_version: Option<String>,
 
         /// Build a crate at a specific path
-        #[structopt(short = "l", long = "local", conflicts_with_all(&["CRATE_NAME", "CRATE_VERSION"]))]
+        #[structopt(
+            short = "l",
+            long = "local",
+            conflicts_with_all(&["CRATE_NAME", "CRATE_VERSION", "CRATE_REGISTRY"])
+        )]
         local: Option<PathBuf>,
     },
 
