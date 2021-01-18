dbda1db2433d5eb0f53c86decd5a196f16d28c37
Pietro Albini <pietro@pietroalbini.org>
2021-01-18 18:14:57 +0100

    statics: remove pure-css from the static search path
    Those CSS files are now included in vendored.css, so there is no need to
    serve them through /-/static anymore.

--- src/web/statics.rs
+++ src/web/statics.rs
@@ -12,7 +12,7 @@ use std::{ffi::OsStr, fs, path::Path};
 const VENDORED_CSS: &str = include_str!(concat!(env!("OUT_DIR"), "/vendored.css"));
 const STYLE_CSS: &str = include_str!(concat!(env!("OUT_DIR"), "/style.css"));
 const RUSTDOC_CSS: &str = include_str!(concat!(env!("OUT_DIR"), "/rustdoc.css"));
-const STATIC_SEARCH_PATHS: &[&str] = &["vendor/pure-css/css", "static", "vendor"];
+const STATIC_SEARCH_PATHS: &[&str] = &["static", "vendor"];
 
 pub(crate) fn static_handler(req: &mut Request) -> IronResult<Response> {
     let mut file = req.url.path();
@@ -252,7 +252,7 @@ mod tests {
         wrapper(|env| {
             let web = env.frontend();
 
-            let files = &[("pure-min.css", "text/css")];
+            let files = &[("highlightjs/styles/dark.min.css", "text/css")];
 
             for (file, mime) in files {
                 let url = format!("/-/static/{}", file);
