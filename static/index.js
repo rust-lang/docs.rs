e98c8a6733a74e79efc4177bba43feb1a4305f68
Michael Howell <michael@notriddle.com>
2021-05-04 15:54:33 -0700

    Reset button from checkmark to copy icon
    This is, again, directly copying what rustdoc does.

--- static/index.js
+++ static/index.js
@@ -3,30 +3,40 @@ function formatCrateName(crateTitleAndVersion) {
     return stringParts[0] + ' = "' + stringParts[1] + '"';
 }
 
-function copyTextHandler() {
-    const crateTitleAndVersion = document.getElementById("crate-title");
-    // On rustdoc pages, we use `textTransform: uppercase`, which copies as uppercase.
-    // To avoid that, reset the styles temporarily.
-    const oldTransform = crateTitleAndVersion.style.textTransform;
-    crateTitleAndVersion.style.textTransform = "none";
-    const temporaryInput = document.createElement("input");
-
-    temporaryInput.type = "text";
-    temporaryInput.value = formatCrateName(crateTitleAndVersion.innerText);
-
-    document.body.append(temporaryInput);
-    temporaryInput.select();
-    document.execCommand("copy");
-
-    temporaryInput.remove();
-    crateTitleAndVersion.style.textTransform = oldTransform;
-
-    const clipboard = document.getElementById("clipboard");
-    clipboard.innerHTML = "✓";
-}
-
 (function() {
     const clipboard = document.getElementById("clipboard");
+    let resetClipboardTimeout = null;
+
+    function resetClipboard() {
+        resetClipboardTimeout = null;
+        clipboard.textContent = '⎘';
+    }
+
+    function copyTextHandler() {
+        const crateTitleAndVersion = document.getElementById("crate-title");
+        // On rustdoc pages, we use `textTransform: uppercase`, which copies as uppercase.
+        // To avoid that, reset the styles temporarily.
+        const oldTransform = crateTitleAndVersion.style.textTransform;
+        crateTitleAndVersion.style.textTransform = "none";
+        const temporaryInput = document.createElement("input");
+
+        temporaryInput.type = "text";
+        temporaryInput.value = formatCrateName(crateTitleAndVersion.innerText);
+
+        document.body.append(temporaryInput);
+        temporaryInput.select();
+        document.execCommand("copy");
+
+        temporaryInput.remove();
+        crateTitleAndVersion.style.textTransform = oldTransform;
+
+        clipboard.textContent = "✓";
+        if (resetClipboardTimeout !== null) {
+            clearTimeout(resetClipboardTimeout);
+        }
+        resetClipboardTimeout = setTimeout(resetClipboard, 1000);
+    }
+
     if (clipboard != null) clipboard.addEventListener("click", copyTextHandler);
     for (const e of document.querySelectorAll('a[data-fragment="retain"]')) {
         e.addEventListener('mouseover', () => e.hash = document.location.hash);
