e3701a7ba38e92a1ff62635d82600bc7f86e8170
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-10-10 22:24:19 +0800

    remove git2 from dependencies

--- src/index/mod.rs
+++ src/index/mod.rs
@@ -1,6 +1,9 @@
+use std::sync::atomic::AtomicBool;
 use std::{path::PathBuf, process::Command};
 
 use anyhow::Context;
+use crates_index_diff::git;
+use crates_index_diff::git::prelude::ObjectIdExt;
 use url::Url;
 
 use self::api::Api;
@@ -26,31 +29,33 @@ struct IndexConfig {
 /// repository has a remote called `origin` and that the branch `master` exists on it.
 ///
 /// [RFC 2141]: https://rust-lang.github.io/rfcs/2141-alternative-registries.html
-fn load_config(repo: &git2::Repository) -> Result<IndexConfig> {
-    let tree = repo
-        .find_commit(repo.refname_to_id("refs/remotes/origin/master")?)?
-        .tree()?;
-    let file = tree
-        .get_name("config.json")
-        .with_context(|| anyhow::anyhow!("registry index missing config"))?;
-    let config = serde_json::from_slice(repo.find_blob(file.id())?.content())?;
+fn load_config(repo: &git::Repository) -> Result<IndexConfig> {
+    let file = repo
+        .rev_parse_single("refs/remotes/origin/master")?
+        .object()?
+        .try_into_commit()?
+        .tree()?
+        .lookup_entry_by_path("config.json")?
+        .with_context(|| anyhow::anyhow!("registry index missing config"))?
+        .oid
+        .attach(&repo)
+        .object()?;
+
+    let config = serde_json::from_slice(&file.data)?;
     Ok(config)
 }
 
 impl Index {
-    pub fn from_url(path: PathBuf, repository_url: String) -> Result<Self> {
-        let url = repository_url.clone();
+    pub fn from_url(path: PathBuf, url: String) -> Result<Self> {
         let diff = crates_index_diff::Index::from_path_or_cloned_with_options(
             &path,
-            crates_index_diff::index::CloneOptions {
-                repository_url,
-                ..Default::default()
-            },
+            git::progress::Discard,
+            &AtomicBool::default(),
+            crates_index_diff::index::CloneOptions { url: url.clone() },
         )
         .context("initialising registry index repository")?;
 
-        let config = load_config(&git2::Repository::open(diff.repository().git_dir())?)
-            .context("loading registry config")?;
+        let config = load_config(diff.repository()).context("loading registry config")?;
         let api = Api::new(config.api).context("initialising registry api client")?;
         Ok(Self {
             path,
@@ -64,8 +69,7 @@ impl Index {
         // See https://github.com/rust-lang/docs.rs/pull/847
         let diff = crates_index_diff::Index::from_path_or_cloned(&path)
             .context("initialising registry index repository")?;
-        let config = load_config(&git2::Repository::open(diff.repository().git_dir())?)
-            .context("loading registry config")?;
+        let config = load_config(diff.repository()).context("loading registry config")?;
         let api = Api::new(config.api).context("initialising registry api client")?;
         Ok(Self {
             path,
@@ -78,13 +82,15 @@ impl Index {
         let options = self
             .repository_url
             .clone()
-            .map(|repository_url| crates_index_diff::index::CloneOptions {
-                repository_url,
-                ..Default::default()
-            })
+            .map(|url| crates_index_diff::index::CloneOptions { url })
             .unwrap_or_default();
-        let diff = crates_index_diff::Index::from_path_or_cloned_with_options(&self.path, options)
-            .context("re-opening registry index for diff")?;
+        let diff = crates_index_diff::Index::from_path_or_cloned_with_options(
+            &self.path,
+            git::progress::Discard,
+            &AtomicBool::default(),
+            options,
+        )
+        .context("re-opening registry index for diff")?;
         Ok(diff)
     }
 
