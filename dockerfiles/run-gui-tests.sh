3851bcc343d8ffc096599210a1a873858f14c0b7
Guillaume Gomez <guillaume1.gomez@gmail.com>
2023-11-03 16:10:42 +0100

    Fix CI compilation error

--- dockerfiles/run-gui-tests.sh
+++ dockerfiles/run-gui-tests.sh
@@ -7,18 +7,30 @@ docker-compose stop web
 
 docker-compose up -d db s3
 
+# If we have a .env file, we need to temporarily move it so
+# it doesn't make sqlx fail compilation.
+if [ -f .env ]; then
+  mv .env .tmp.env
+fi
+
 # We add the information we need.
 cargo run -- database migrate
+cargo run -- build update-toolchain
 cargo run -- build crate sysinfo 0.23.4
 cargo run -- build crate sysinfo 0.23.5
 cargo run -- build add-essential-files
 
+if [ -f .tmp.env ]; then
+  mv .tmp.env .env
+fi
+
 # In case we don't have a `.env`, we create one.
 if [ ! -f .env ]; then
-cp .env.sample .env
+  cp .env.sample .env
 fi
 
 . .env
+
 cargo run -- start-web-server &
 SERVER_PID=$!
 
