3f7b37ba3c6f273ee3bc1333564643e32cdc88b2
Wim Looman <git@nemo157.com>
2024-03-06 14:11:37 +0100

    Embed metadata about the current page instead of reading from the rendered html

--- static/index.js
+++ static/index.js
@@ -1,8 +1,3 @@
-function formatCrateName(crateTitleAndVersion) {
-    const stringParts = crateTitleAndVersion.split(" ", 2);
-    return stringParts[0] + ' = "' + stringParts[1] + '"';
-}
-
 (function() {
     const clipboard = document.getElementById("clipboard");
     if (clipboard) {
@@ -14,23 +9,17 @@ function formatCrateName(crateTitleAndVersion) {
             clipboard.innerHTML = resetClipboardIcon;
         }
 
-        function copyTextHandler() {
-            const crateTitleAndVersion = document.getElementById("crate-title");
-            // On rustdoc pages, we use `textTransform: uppercase`, which copies as uppercase.
-            // To avoid that, reset the styles temporarily.
-            const oldTransform = crateTitleAndVersion.style.textTransform;
-            crateTitleAndVersion.style.textTransform = "none";
-            const temporaryInput = document.createElement("input");
+        async function copyTextHandler() {
+            const metadata = JSON.parse(document.getElementById("crate-metadata").innerText)
 
+            const temporaryInput = document.createElement("input");
             temporaryInput.type = "text";
-            temporaryInput.value = formatCrateName(crateTitleAndVersion.innerText);
+            temporaryInput.value = `${metadata.name} = "${metadata.version}"`;
 
             document.body.append(temporaryInput);
             temporaryInput.select();
             document.execCommand("copy");
-
             temporaryInput.remove();
-            crateTitleAndVersion.style.textTransform = oldTransform;
 
             clipboard.textContent = "✓";
             if (resetClipboardTimeout !== null) {
