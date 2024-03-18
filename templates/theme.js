8d019983ba2748191291a73aaf25b0098b8e82e5
Wim Looman <git@nemo157.com>
2024-03-18 11:03:44 +0100

    Don't pollute the global function namespace with theme functions

--- templates/theme.js
+++ templates/theme.js
@@ -1,18 +1,20 @@
-function applyTheme(theme) {
-    document.documentElement.dataset.theme = theme;
-}
-
-window.addEventListener('storage', function (ev) {
-    if (ev.key === 'rustdoc-theme') {
-        applyTheme(ev.newValue);
+(function() {
+    function applyTheme(theme) {
+        document.documentElement.dataset.theme = theme;
     }
-});
 
-// see ./storage-change-detection.html for details
-window.addEventListener('message', function (ev) {
-    if (ev.data && ev.data.storage && ev.data.storage.key === 'rustdoc-theme') {
-        applyTheme(ev.data.storage.value);
-    }
-});
+    window.addEventListener('storage', function (ev) {
+        if (ev.key === 'rustdoc-theme') {
+            applyTheme(ev.newValue);
+        }
+    });
 
-applyTheme(window.localStorage.getItem('rustdoc-theme'));
+    // see ./storage-change-detection.html for details
+    window.addEventListener('message', function (ev) {
+        if (ev.data && ev.data.storage && ev.data.storage.key === 'rustdoc-theme') {
+            applyTheme(ev.data.storage.value);
+        }
+    });
+
+    applyTheme(window.localStorage.getItem('rustdoc-theme'));
+})()
