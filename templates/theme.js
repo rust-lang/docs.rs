f8d1f02f412333a239ec70794368a895be3740a6
Wim Looman <git@nemo157.com>
2024-03-18 10:59:14 +0100

    Fix indentation

--- templates/theme.js
+++ templates/theme.js
@@ -1,18 +1,18 @@
 function applyTheme(theme) {
-  document.documentElement.dataset.theme = theme;
+    document.documentElement.dataset.theme = theme;
 }
 
 window.addEventListener('storage', function (ev) {
-  if (ev.key === 'rustdoc-theme') {
-    applyTheme(ev.newValue);
-  }
+    if (ev.key === 'rustdoc-theme') {
+        applyTheme(ev.newValue);
+    }
 });
 
 // see ./storage-change-detection.html for details
 window.addEventListener('message', function (ev) {
-  if (ev.data && ev.data.storage && ev.data.storage.key === 'rustdoc-theme') {
-    applyTheme(ev.data.storage.value);
-  }
+    if (ev.data && ev.data.storage && ev.data.storage.key === 'rustdoc-theme') {
+        applyTheme(ev.data.storage.value);
+    }
 });
 
 applyTheme(window.localStorage.getItem('rustdoc-theme'));
