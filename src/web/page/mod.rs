be8e5710e95c11f6d3b5d40aac801118b4f9fcf4
Guillaume Gomez <guillaume1.gomez@gmail.com>
2024-09-26 21:34:45 +0200

    move fontawesome icons check at compile-time

--- src/web/page/mod.rs
+++ src/web/page/mod.rs
@@ -3,14 +3,29 @@ pub(crate) mod web_page;
 
 pub(crate) use templates::TemplateData;
 
-use serde::Serialize;
+use crate::f_a::IconStr;
+use serde::ser::{Serialize, SerializeStruct, Serializer};
 
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Serialize)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub(crate) struct GlobalAlert {
     pub(crate) url: &'static str,
     pub(crate) text: &'static str,
     pub(crate) css_class: &'static str,
-    pub(crate) fa_icon: &'static str,
+    pub(crate) fa_icon: crate::f_a::icons::IconTriangleExclamation,
+}
+
+impl Serialize for GlobalAlert {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut s = serializer.serialize_struct("GlobalAlert", 4)?;
+        s.serialize_field("url", &self.url)?;
+        s.serialize_field("text", &self.text)?;
+        s.serialize_field("css_class", &self.css_class)?;
+        s.serialize_field("fa_icon", &self.fa_icon.icon_name())?;
+        s.end()
+    }
 }
 
 #[cfg(test)]
@@ -24,14 +39,14 @@ mod tera_tests {
             url: "http://www.hasthelargehadroncolliderdestroyedtheworldyet.com/",
             text: "THE WORLD WILL SOON END",
             css_class: "THE END IS NEAR",
-            fa_icon: "https://gph.is/1uOvmqR",
+            fa_icon: crate::f_a::icons::IconTriangleExclamation,
         };
 
         let correct_json = json!({
             "url": "http://www.hasthelargehadroncolliderdestroyedtheworldyet.com/",
             "text": "THE WORLD WILL SOON END",
             "css_class": "THE END IS NEAR",
-            "fa_icon": "https://gph.is/1uOvmqR"
+            "fa_icon": "triangle-exclamation"
         });
 
         assert_eq!(correct_json, serde_json::to_value(alert).unwrap());
