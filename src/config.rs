3669b650445f66e132ae062973d97ded040d5c5b
Francisco Noel <63328653+byfnoel@users.noreply.github.com>
2025-07-04 14:50:23 -0400

    Dead code warning only for non-linux platforms
    Co-authored-by: Denis Cornehl <denis@cornehl.org>

--- src/config.rs
+++ src/config.rs
@@ -120,7 +120,7 @@ pub struct Config {
     pub(crate) build_default_memory_limit: Option<usize>,
     pub(crate) include_default_targets: bool,
 
-    #[allow(dead_code)]
+    #[cfg_attr(not(target_os = "linux"), allow(dead_code))]
     pub(crate) disable_memory_limit: bool,
 
     // automatic rebuild configuration
