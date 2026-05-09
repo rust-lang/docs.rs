282abbcff7e7cb5e7fc9b83b7fe077b79d40f201
Guillaume Gomez <contact@guillaume-gomez.fr>
2026-05-09 03:21:19 +0200

    Allow to run specific GUI tests by passing the test paths directly

--- gui-tests/tester.js
+++ gui-tests/tester.js
@@ -16,17 +16,30 @@ async function main(argv) {
     if (typeof process.env.NODE_MODULE_PATH !== "undefined") {
         nodeModulePath = process.env.NODE_MODULE_PATH;
     }
-    await spawn("node", [
+
+    const filesToTest = argv.slice(2).filter(v => !v.startsWith("--"));
+    const options = argv.slice(2).filter(v => v.startsWith("--"));
+    const cmd = [
         path.join(nodeModulePath, "browser-ui-test/src/index.js"),
         "--display-format",
         "compact",
         "--variable",
         "DOC_PATH",
         server,
-        "--test-folder",
-        __dirname,
-        ...argv.slice(2),
-    ], {stdio: "inherit", stderr: "inherit"}).on("exit", code => {
+    ];
+    if (filesToTest.length === 0) {
+        cmd.push("--test-folder");
+        cmd.push(__dirname);
+    } else {
+        for (const fileToTest of filesToTest) {
+            cmd.push("--test-file");
+            cmd.push(fileToTest);
+        }
+    }
+    for (const option of options) {
+        cmd.push(option);
+    }
+    await spawn("node", cmd, {stdio: "inherit", stderr: "inherit"}).on("exit", code => {
         if (code !== 0) {
             process.exit(1);
         }
