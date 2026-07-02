0eefa23583d8224dcc84478b726558d6d06b66cc
Denis Cornehl <denis@cornehl.org>
2026-07-02 08:13:21 +0200

    kk

--- crates/lib/docs_rs_crate_zip/src/lib.rs
+++ crates/lib/docs_rs_crate_zip/src/lib.rs
@@ -1,3 +1,10 @@
+//! Library to read the crates.io source archives (manifest & zip), and
+//! fetch single files from the remote archives.
+//!
+//! Archives are created here:
+//! https://github.com/rust-lang/crates.io/blob/5274087feb193ee490e9a6bbdf2e18e74e9ddaeb/crates/crates_io_crate_zip/src/lib.rs
+//! Also we copied the manifest structs from there.
+
 mod error;
 mod manifest;
 mod source_archive;
