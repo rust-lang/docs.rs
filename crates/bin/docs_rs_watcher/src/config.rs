a4150f0b4f4ebf46578f6734b7acc3a6e83098e2
Denis Cornehl <denis@cornehl.org>
2026-08-05 04:18:25 +0200

    kk

--- crates/bin/docs_rs_watcher/src/config.rs
+++ crates/bin/docs_rs_watcher/src/config.rs
@@ -5,6 +5,29 @@ use std::{path::PathBuf, time::Duration};
 use url::Url;
 
 #[derive(Debug)]
+pub struct SqsConfig {
+    pub queue_url: Url,
+    pub region: String,
+    pub endpoint_url: Url,
+    pub max_retries: u32,
+    /// temporary, to switch between the sources for the index (git index vs SQS)
+    pub active: bool,
+}
+
+impl AppConfig for SqsConfig {
+    fn from_environment() -> Result<Self> {
+        let prefix: PathBuf = require_env("DOCSRS_PREFIX")?;
+        Ok(Self {
+            queue_url: mandatory_env("DOCSRS_SQS_QUEUE_URL")?,
+            region: mandatory_env("DOCSRS_SQS_REGION")?,
+            endpoint_url: mandatory_env("DOCSRS_SQS_ENDPOINT_URL")?,
+            active: env("DOCSRS_SQS_ACTIVE", false)?,
+            max_retries: env("DOCSRS_MAX_RETRIES", 6u32)?,
+        })
+    }
+}
+
+#[derive(Debug)]
 pub struct Config {
     /// registry watching config. Also used for database-synchonize
     pub registry_index_path: PathBuf,
@@ -14,19 +37,13 @@ pub struct Config {
     // Time between 'git gc --auto' calls in seconds
     pub registry_gc_interval: u64,
 
-    /// SQS watching config.
-    pub sqs_queue_url: Option<Url>,
-    pub sqs_region: Option<String>,
-    pub sqs_endpoint_url: Option<Url>,
-    /// temporary, to switch between the sources for the index (git index vs SQS)
-    pub sqs_active: bool,
-    pub aws_sdk_max_retries: u32,
-
     // automatic rebuild configuration
     pub max_queued_rebuilds: Option<u16>,
 
     /// Maximum time to wait for queue row locks when deleting crates/releases.
     pub delete_lock_timeout: Duration,
+
+    pub crates_io_events: Option<SqsConfig>,
 
     pub repository: docs_rs_repository_stats::Config,
 }
@@ -38,11 +55,7 @@ impl AppConfig for Config {
             registry_index_path: env("REGISTRY_INDEX_PATH", prefix.join("crates.io-index"))?,
             registry_url: maybe_env("REGISTRY_URL")?,
 
-            sqs_queue_url: maybe_env("DOCSRS_SQS_QUEUE_URL")?,
-            sqs_region: maybe_env("DOCSRS_SQS_REGION")?,
-            sqs_endpoint_url: maybe_env("DOCSRS_SQS_ENDPOINT_URL")?,
-            sqs_active: env("DOCSRS_SQS_ACTIVE", false)?,
-            aws_sdk_max_retries: env("DOCSRS_AWS_SDK_MAX_RETRIES", 6u32)?,
+            crates_io_events: Arc::new(SqsConfig::from_environment()?),
 
             delay_between_registry_fetches: Duration::from_secs(env::<u64>(
                 "DOCSRS_DELAY_BETWEEN_REGISTRY_FETCHES",
@@ -61,7 +74,7 @@ impl AppConfig for Config {
     #[cfg(test)]
     fn test_config() -> Result<Self> {
         let mut config = Self::from_environment()?;
-        config.sqs_active = false;
+        config.crates_io_events = None;
         Ok(config)
     }
 }
