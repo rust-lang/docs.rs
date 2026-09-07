b296f6d694cd8a8e886689de2b361bd8f46d2d33
Denis Cornehl <denis@cornehl.org>
2026-09-07 10:40:24 +0200

    admin cli: remove s3-cleanup command

--- crates/bin/docs_rs_admin/src/main.rs
+++ crates/bin/docs_rs_admin/src/main.rs
@@ -1,4 +1,3 @@
-mod cleanup_s3;
 mod rebuilds;
 #[cfg(test)]
 pub(crate) mod testing;
@@ -373,12 +372,6 @@ enum DatabaseSubcommand {
     /// Updates GitHub/GitLab stats for crates.
     UpdateRepositoryFields,
 
-    /// Clean up the s3 bucket from non-archive storage files.
-    CleanS3Bucket {
-        #[arg(long)]
-        dry_run: bool,
-    },
-
     /// Backfill GitHub/GitLab stats for crates.
     BackfillRepositoryStats,
 
@@ -440,14 +433,6 @@ impl DatabaseSubcommand {
 
                 println!("update repository stats where outdated...");
                 ctx.repository_stats()?.update_all_crates().await?;
-            }
-
-            Self::CleanS3Bucket { dry_run } => {
-                println!("clean up s3 bucket...");
-                let mut conn = ctx.pool()?.get_async().await?;
-                let storage = ctx.storage()?;
-
-                cleanup_s3::cleanup_s3_bucket(&mut conn, storage, dry_run).await?;
             }
 
             Self::BackfillRepositoryStats => {
