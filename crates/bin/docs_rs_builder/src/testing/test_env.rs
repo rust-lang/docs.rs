27fae8e68ef559988f8feb072904be568133d4a5
Denis Cornehl <denis@cornehl.org>
2026-09-08 12:33:22 +0200

    kk

--- crates/bin/docs_rs_builder/src/testing/test_env.rs
+++ crates/bin/docs_rs_builder/src/testing/test_env.rs
@@ -31,7 +31,11 @@ pub(crate) trait TestEnvironmentExt {
 
 impl TestEnvironmentExt for TestEnvironment {
     fn build_builder(&self) -> Result<TestBuilder> {
-        crate::logging::init(&docs_rs_logging::Config::test_config()?); // initialize rustwide logging
+        // initialize rustwide logging
+        let logging_config = docs_rs_logging::Config::test_config()?;
+        docs_rs_rustwide::logging::init(logging_config.log_build_logs);
+
+        // initialize workspace
         let workspace = TestWorkspace::acquire_at(&self.config().rustwide_workspace)?;
         let builder = RustwideBuilder::init(self.config().clone(), self)?;
         Ok(TestBuilder {
