0861ee4dc218e6d80004791b91b4c8386564217f
Denis Cornehl <denis@cornehl.org>
2026-09-09 12:02:33 +0200

    testing

--- crates/lib/docs_rs_rustwide/src/testing.rs
+++ crates/lib/docs_rs_rustwide/src/testing.rs
@@ -6,16 +6,14 @@ use std::path::{Path, PathBuf};
 pub const TEST_SANDBOX_IMAGE: &str = "ghcr.io/rust-lang/crates-build-env/linux-micro";
 
 /// Persistent workspace used by integration tests. BuildEnvironment owns its lock.
+///
+/// For better build speed, should be shared across tests using the rustwide builder:
+/// * `docs_rs_rustwide` lib integration tests
+/// * `docs_rs_builder` bin build-tests
 pub fn test_workspace_path() -> PathBuf {
     std::env::var_os("DOCSRS_RUSTWIDE_WORKSPACE")
         .map(PathBuf::from)
-        .unwrap_or_else(|| {
-            Path::new(env!("CARGO_MANIFEST_DIR"))
-                .ancestors()
-                .nth(3)
-                .expect("docs_rs_rustwide must be inside the workspace")
-                .join(".workspace")
-        })
+        .unwrap_or_else(|| Path::new(env!("CARGO_MANIFEST_DIR")).join(".workspace"))
 }
 
 pub fn test_sandbox_image() -> SandboxImageSource {
