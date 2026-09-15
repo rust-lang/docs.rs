8702f37d880086df05ae931736e04da026c38889
Denis Cornehl <denis@cornehl.org>
2026-09-15 09:52:33 +0200

    kll

--- crates/bin/docs_rs_build/src/main.rs
+++ crates/bin/docs_rs_build/src/main.rs
@@ -66,7 +66,7 @@ fn run(args: &Args) -> Result<bool> {
     let krate = Crate::local(packaged.path());
     let build = environment
         .release(&krate)
-        .run(|release| release.build_docs())
+        .run(|release| Ok(release.build_docs()))
         .context("running the docs.rs build")?;
     let duration = build.duration();
     let result = build.into_inner();
